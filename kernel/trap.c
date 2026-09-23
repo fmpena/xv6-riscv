cf25b26fa6a0a0793cb9e49d053777ca8d51bbfc
Frida Peña <fmpena@miners.utep.edu>
2026-09-23 13:26:37 -0700

    Track process CPU time

--- kernel/trap.c
+++ kernel/trap.c
@@ -81,9 +81,11 @@ usertrap(void)
   if (killed(p))
     kexit(-1);
 
-  // give up the CPU if this is a timer interrupt.
-  if (which_dev == 2)
+  if(which_dev == 2){
+    //count cpu time
+    p->cputime++;
     yield();
+  }
 
   prepare_return();
 
@@ -153,9 +155,11 @@ kerneltrap()
     panic("kerneltrap");
   }
 
-  // give up the CPU if this is a timer interrupt.
-  if (which_dev == 2 && myproc() != 0)
+  if(which_dev == 2 && myproc() != 0){
+    //count cpu time
+    myproc()->cputime++;
     yield();
+  }
 
   // the yield() may have caused some traps to occur,
   // so restore trap registers for use by kernelvec.S's sepc instruction.
